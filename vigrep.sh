3ff7dc3d9618081418902dd49fbfbb73337c45df
Will Jaspers <wjaspers4@gmail.com>
2014-10-08 16:24:31 -0500

    Fix some mistakes.
    - ensure params are tested properly.
    - ensure cut didn't barf out ":Filename too long"
      - this only seems to happen when the script is called from within fish.
      - i dont really plan on digging deeply into it.
      - bail out if the excess string is found.

--- vigrep.sh
+++ vigrep.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
-if [ -z $1 ]; then
+if [[ -z $1 ]]; then
 	echo "Please specify a search string".
 	echo
 	exit 1
 fi
 
-if [ -z $2 ]; then
-	echo "Please specify a directory to search."
+if [[ -z $2 ]]; then
+        echo "Please specify a directory to search."
+        echo
+        exit 1
+fi
+
+RESULT=$(grep -rl "$1" "$2")
+
+if [ $? -gt 0 ]; then
+	echo "Your search did not produce a useable result."
 	echo
 	exit 1
 fi
 
-RESULT=$(grep -rH "$1" "$2" | cut -d ":" -f 1)
 
-if [ $? -gt 0 ]; then
-	echo "Your search did not produce a useable result."
-	echo "$RESULT"
+TEST_RESULT=$(echo "$RESULT" | cut -s -d ":" -f 1)
+if [ $TEST_RESULT ] && [[ $TEST_RESULT=~'too long' ]]; then
+	echo "Your search produced too many results for vi to handle."
 	echo
 	exit 1
 fi
